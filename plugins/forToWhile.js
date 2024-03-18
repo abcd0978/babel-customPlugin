a9bec0c732052fc17ae536df0de4dcd11ba18961
kimmingyu <mgkim@alux-platform.com>
2024-03-18 17:50:42 +0900

    chore: console.log 삭제

--- plugins/forToWhile.js
+++ plugins/forToWhile.js
@@ -5,13 +5,11 @@ module.exports = function(babel){
             ForStatement(path) {
                 if(t.isForStatement(path.node)){
                     const {init, test, update, body} = path.node;
-                    console.log('from forToWhile', update);
                     const updateStatement = t.expressionStatement(update)
                     const newBody = t.blockStatement([
                         ...body.body, updateStatement
                     ]);
                     path.replaceWith(t.blockStatement([init, t.whileStatement(test, newBody)]))
-                    //console.log('body Expression', body.body);
                 }
             }
         }
